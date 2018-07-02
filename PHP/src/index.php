9cbfd99de28c172dbf4b13f8c12d943fe8ce1d49
MatheoDev404 <stunaultmatheo.dev@gmail.com>
2018-07-02 11:15:07 +0200

    Constantes 02-07-2018

--- PHP/src/index.php
+++ PHP/src/index.php
@@ -10,8 +10,8 @@
 
     <?php
 
-    echo 'hello world';
-    echo '</br><strong> du text </strong>';
+        echo 'hello world';
+        echo '</br><strong> du text </strong>';
 
     ?>
 
@@ -19,40 +19,51 @@
 
     <?php
 
-    // déclaration de variable
-    $a = 1; // type INTEGER ou INT
-    $b = 1.5; // type FLOAT
-    $c ='hello'; //type STRING
-    $d = true; //type BOOLEAN ou BOOL
-    
-    // echo affiche la valeur de la variable.
-    echo $a;
-    echo '<br>';
-    
-    // var_dump() affiche les informations structurées d'une variable, y compris son type et sa valeur.
-    var_dump($b);
-    echo '<br>';
+        // déclaration de variable
+        $a = 1; // type INTEGER ou INT
+        $b = 1.5; // type FLOAT
+        $c ='hello'; //type STRING
+        $d = true; //type BOOLEAN ou BOOL
+        
+        // echo affiche la valeur de la variable.
+        echo $a;
+        echo '<br>';
+        
+        // var_dump() : Affiche les informations structurées d'une variable, y compris son type et sa valeur.
+        var_dump($b);
+        echo '<br>';
 
-    $e = (int)$d; // pour forcer le type d'une variable.
-    var_dump($e);
-    echo '<br>';
-    
-    // concaténation avec le ' . ' 
-    echo $a . $b;
-    echo '</br>' . $a . ' to the ' . $b;
+        $e = (int)$d; // pour forcer le type d'une variable.
+        var_dump($e);
+        echo '<br>';
+        
+        // concaténation avec le ' . ' 
+        echo $a . $b;
+        echo '</br>' . $a . ' to the ' . $b;
 
-    $c = 2;
-    // devient une string 'Un plus un font 2'
-    $d = 'Un plus un font ' . 2;
-    echo '<br>' . $d
+        $c = 2;
+        // devient une string 'Un plus un font 2'
+        $d = 'Un plus un font ' . 2;
+        echo '<br>' . $d
+
     ?>
 
     <h2>Guillemets</h2>
 
     <?php 
-    $a = 'Bonjour';
-    echo '$a le monde'; // simple quote, la variable n'est pas interpr
-    echo "</br>$a le monde";// double quote, la variable n'est pas interpr
+
+        $a = 'Bonjour';
+        echo '$a le monde'; // simple quote, la variable n'est pas interprété.
+        echo "</br>$a le monde";// double quote, la variable est interprété.
+
+    ?>
+
+    <h2>Constantes</h2>
+    
+    <?php 
+    // define() : Définit une constante
+    define('VILLE', 'Paris');
+
     ?>
 </body>
 <script src="assets/js/main.js"></script>
