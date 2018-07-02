1b28be3e6a2443bdeb04f844b1a1813c99bd673d
MatheoDev404 <stunaultmatheo.dev@gmail.com>
2018-07-02 11:11:36 +0200

    quotes

--- PHP/src/index.php
+++ PHP/src/index.php
@@ -19,18 +19,41 @@
 
     <?php
 
-    //    déclaration de variable
+    // déclaration de variable
     $a = 1; // type INTEGER ou INT
-    
     $b = 1.5; // type FLOAT
     $c ='hello'; //type STRING
     $d = true; //type BOOLEAN ou BOOL
     
-    // affiche la valeur de la variable
+    // echo affiche la valeur de la variable.
     echo $a;
+    echo '<br>';
     
+    // var_dump() affiche les informations structurées d'une variable, y compris son type et sa valeur.
+    var_dump($b);
+    echo '<br>';
+
+    $e = (int)$d; // pour forcer le type d'une variable.
+    var_dump($e);
+    echo '<br>';
+    
+    // concaténation avec le ' . ' 
+    echo $a . $b;
+    echo '</br>' . $a . ' to the ' . $b;
+
+    $c = 2;
+    // devient une string 'Un plus un font 2'
+    $d = 'Un plus un font ' . 2;
+    echo '<br>' . $d
     ?>
 
+    <h2>Guillemets</h2>
+
+    <?php 
+    $a = 'Bonjour';
+    echo '$a le monde'; // simple quote, la variable n'est pas interpr
+    echo "</br>$a le monde";// double quote, la variable n'est pas interpr
+    ?>
 </body>
 <script src="assets/js/main.js"></script>
 </html>
