f5105cb90a74691e846b4969dbc6b6b3266960ad
MatheoDev404 <stunaultmatheo.dev@gmail.com>
2018-07-03 17:31:34 +0200

    super globales 03-07-2018

--- PHP/src/get/index.php
+++ PHP/src/get/index.php
@@ -1,1 +1,5 @@
-{\rtf1}
+<a href="page2.php?nom=Stunault&prenom=Mathéo&langage=PHP">Vers page 2</a>
+ <!-- 
+    la partie de l'url qui commence par ? s'appelle la query string.
+    elle contient des informations sous forme de clé=valeur séparées elle m^me par &
+ -->
---
+++ PHP/src/get/page2.php
@@ -0,0 +1,13 @@
+<?php
+var_dump($_GET);
+
+echo '<br/>';
+echo $_GET['prenom'] . ' ' . $_GET['nom'];
+
+function test(){
+    var_dump($_GET);
+    
+}
+
+echo '<br/>';
+test();
